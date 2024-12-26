65468d59b4bf9d39ff416a465b96957deb1da46a
Alejandrozic <alejandrozic@gmail.com>
2024-12-26 08:41:42 -0500

    Create emit-from-child-component-to-parent.ts

---
+++ snippets/emit-from-child-component-to-parent.ts
@@ -0,0 +1,44 @@
+////////////////////////////////////////
+// Child TypeScript
+
+import { Component, EventEmitter, Output } from '@angular/core';
+...
+export class ChildComponent {
+  @Output() cancel =  new EventEmitter<string>();                         // <== 1
+
+  onCancelBtn() {
+    this.cancel.emit();                                                   // <== 2
+  }
+}
+
+////////////////////////////////////////
+// Child HTML
+
+// <div class="backdrop"></div>
+// <dialog open>
+//     ...
+//     <p class="actions">
+//       <button type="button" (click)="onCancelBtn()">Cancel</button>    // <== 3
+//     </p>
+// </dialog>
+
+////////////////////////////////////////
+// Parent TypeScript
+
+import { Component } from '@angular/core';
+...
+export class ParentComponent {
+  isAddingTask: boolean = false;                                         // <== 4
+
+  onCancelBtn() {
+    this.isAddingTask = false;                                           // <== 5
+  }
+
+}
+
+////////////////////////////////////////
+// Parent HTML
+
+// @if (isAddingTask) {
+//   <app-new-task (cancel)="onCancelBtn()"/>                          // <== 6
+// }
